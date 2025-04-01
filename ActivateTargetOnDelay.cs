fe0ed360758931c18139ffc92166b32aa24ef64a
Gregg Whitworth <whitwort@gmail.com>
2025-04-01 12:14:20 -0400

    Create ActivateTargetOnDelay.cs

---
+++ ActivateTargetOnDelay.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateTarget: MonoBehaviour
+{
+    public GameObject target;
+    public bool activate = true;
+    public int delay = 3;
+
+    void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(StartDelay());
+    }
+
+    IEnumerator StartDelay()
+    {
+        yield return new WaitForSeconds(delay);
+        Debug.Log("fire delayed activation: " + GameObject);
+        target.SetActive(activate);
+    }
+
+
+}
