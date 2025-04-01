2ab4ec0fda81d5be9363533048039c7c85e751c3
Gregg Whitworth <whitwort@gmail.com>
2025-04-01 12:16:06 -0400

    Create ActivateTargetOnTagOnDelay.cs

---
+++ ActivateTargetOnTagOnDelay.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateTargetOnTagOnDelay: MonoBehaviour
+{
+    public GameObject target;
+    public bool activate = true;
+    public int delay = 3;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(collidingTag))
+        {
+          StartCoroutine(StartDelay());
+        }
+        
+    }
+
+    IEnumerator StartDelay()
+    {
+        yield return new WaitForSeconds(delay);
+        Debug.Log("fire delayed activation: " + GameObject);
+        target.SetActive(activate);
+    }
+
+
+}
